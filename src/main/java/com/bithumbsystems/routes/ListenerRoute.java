5ee681255cfcd65ba136f53c629234bbce4458b7
hgjeong <hgjeong@bithumbsystems.com>
2022-09-17 16:38:07 +0900

    다우메신저, 텔레그램 테스트 소스 제거

--- src/main/java/com/bithumbsystems/routes/ListenerRoute.java
+++ src/main/java/com/bithumbsystems/routes/ListenerRoute.java
@@ -2,10 +2,8 @@ package com.bithumbsystems.routes;
 
 import com.bithumbsystems.config.Config;
 import com.bithumbsystems.config.properties.UrlProperties;
-import com.bithumbsystems.filter.MessengerApiFilter;
 import com.bithumbsystems.filter.ReCaptchaApiFilter;
 import com.bithumbsystems.filter.SlackApiFilter;
-import com.bithumbsystems.filter.TelegramApiFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -20,9 +18,6 @@ public class ListenerRoute {
 
     private final UrlProperties urlProperties;
     private final ReCaptchaApiFilter reCaptchaApiFilter;
-    private final MessengerApiFilter messengerApiFilter;
-
-    private final TelegramApiFilter telegramApiFilter;
     private final SlackApiFilter slackApiFilter;
 
     @Bean
@@ -32,16 +27,6 @@ public class ListenerRoute {
                         route -> route.path("/recaptcha/**")
                                 .filters(filter -> filter.filter(reCaptchaApiFilter.apply(new Config("ReCaptcha apply"))))
                                 .uri(urlProperties.getRecaptchaUrl())
-                )
-                .route("messenger-filter",   // 다우메신저(모바일 그룹웨어)
-                        route -> route.path("/messenger/**")
-                                .filters(filter -> filter.filter(messengerApiFilter.apply(new Config("Messenser apply"))))
-                                .uri(urlProperties.getMessengerUrl())
-                )
-                .route("telegram-filter",   // 텔레그램
-                        route -> route.path("/telegram/**")
-                                .filters(filter -> filter.filter(telegramApiFilter.apply(new Config("Telegram apply"))))
-                                .uri(urlProperties.getTelegramUrl())
                 )
                 .route("slack-filter",   // 텔레그램
                         route -> route.path("/slack/**")
